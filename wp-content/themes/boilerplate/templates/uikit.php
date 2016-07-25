3fe1721c6660fe24f494dda2534f3c8dd3885cc4
tracyhebrew <tracy@fiftyandfifty.org>
2016-07-25 14:01:50 -0700

    add uikit template

---
+++ wp-content/themes/boilerplate/templates/uikit.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+ * Template Name: UI Kit
+ * 
+ */
+get_header(); ?>
+
+
+
+<?php
+/**
+ * UI Kit Page Builder Content
+ */
+do_action( 'wds_page_builder_load_parts' ); ?>
+
+
+
+<?php get_footer(); ?>
